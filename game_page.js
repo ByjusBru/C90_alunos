62fe53dc10117a845814c66d55507620ef9a0f72
Bruna Lekich Louro <brunalekich@gmail.com>
2022-10-27 10:47:36 -0300

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,54 @@
+//Completar:
+	player1Name = localStorage.getItem("player1Name");
+	
+
+	player1Score = 0;
+	
+
+
+//ADICIONAR CÓDIGO PARA DOIS PONTOS E TURNO DA PERGUNTA:
+
+
+//FUNCTION ENVIAR:
+function send() {
+
+
+}
+
+
+
+questionTurn = "player1";
+answerTurn = "player2";
+
+
+function check()
+{
+	
+//código
+
+	if(answer == word)	
+	{
+//código
+
+	}
+	if(questionTurn == "player1")
+	{
+	//código	
+    }
+	else 
+	{
+		//código
+	}
+
+	if(answerTurn == "player1")
+	{
+		//código
+	}
+	else 
+	{
+		//código
+	}
+
+    document.getElementById("output").innerHTML = "";
+}
+
